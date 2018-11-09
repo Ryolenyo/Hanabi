dee7664d107ae3ddd840c2ca84663b909cfbc488
Ryolenyo <ryolenyo@gmail.com>
2018-11-09 23:21:02 +0700

    add note speed

---
+++ Assets/Script/Spawn_note.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_note : MonoBehaviour {
+
+	List<float> Note = new List<float>() {1,2,3,4,1,2,3,4};
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
